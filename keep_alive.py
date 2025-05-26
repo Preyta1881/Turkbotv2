b5c614c1400adb4533305bfa18018115a635240d
Preyta1881 <furkankecili366@gmail.com>
2025-05-26 18:36:54 +0300

    keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,6 +1,6 @@
+import os
 from flask import Flask
 from threading import Thread
-import os  # <-- eklendi
 
 app = Flask(__name__)
 
@@ -9,7 +9,7 @@ def home():
     return "Bot aktif!"
 
 def run():
-    port = int(os.environ.get("PORT", 8080))  # <-- dinamik port
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
